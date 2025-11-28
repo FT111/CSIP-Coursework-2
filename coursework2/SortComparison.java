a41a94b4ff89bb1bd694e746a7b66f07822b7b83
FT111 <thetaylor2016@gmail.com>
2025-11-28 12:16:18 +0000

    Adds java base file

---
+++ coursework2/SortComparison.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class SortComparison {
+    static int cardCompare(String card1, String card2) {
+
+    }
+
+    static ArrayList<String> bubbleSort(ArrayList<String> array) {
+
+    }
+
+    static ArrayList<String> mergeSort(ArrayList<String> array) {
+
+    }
+}
