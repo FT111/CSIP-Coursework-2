86d1a5d146b4608f672bfd7f163017f107b4f533
FT111 <thetaylor2016@gmail.com>
2025-12-02 14:17:51 +0000

    Adds card comparison base

--- coursework2/SortComparison.java
+++ coursework2/SortComparison.java
@@ -1,15 +1,21 @@
-import java.util.ArrayList;
+import java.util.Map;
 
 public class SortComparison {
+    private final Map<String, Integer> SuitRanks = Map.of(
+            "H", 1,
+            "C", 2,
+            "D", 3,
+            "S", 4
+    );
+
     static int cardCompare(String card1, String card2) {
-
+        var card1Map = this.SuitRanks[card1[0]];
     }
 
-    static ArrayList<String> bubbleSort(ArrayList<String> array) {
-
-    }
-
-    static ArrayList<String> mergeSort(ArrayList<String> array) {
-
-    }
+//    static ArrayList<String> bubbleSort(ArrayList<String> array) {
+//    }
+//
+//    static ArrayList<String> mergeSort(ArrayList<String> array) {
+//
+//    }
 }
