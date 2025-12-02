883058a54f468b9da1f4c4693c801bad1bf5c5c9
Freddie Taylor <freddiejljtaylor@gmail.com>
2025-12-02 16:56:23 +0000

    Adds card comparison function with test function

--- coursework2/SortComparison.java
+++ coursework2/SortComparison.java
@@ -1,15 +1,25 @@
 import java.util.Map;
 
 public class SortComparison {
-    private final Map<String, Integer> SuitRanks = Map.of(
-            "H", 1,
-            "C", 2,
-            "D", 3,
-            "S", 4
+    private static final Map<Character, Integer> SuitRanks = Map.of(
+            'H', 100,
+            'C', 200,
+            'D', 300,
+            'S', 400
     );
 
+    static int getCardValue(String card) {
+        char[] cardCharArray = card.toCharArray();
+        var cardSuitValue = SuitRanks.get(cardCharArray[cardCharArray.length-1]);
+        String cardValueString = "";
+        cardValueString += cardCharArray[0];
+        if (cardCharArray.length == 3) { cardValueString += cardCharArray[1]; }
+
+        return cardSuitValue + Integer.parseInt(cardValueString);
+    }
+
     static int cardCompare(String card1, String card2) {
-        var card1Map = this.SuitRanks[card1[0]];
+        return getCardValue(card2) > getCardValue(card1) ? -1 : 1;
     }
 
 //    static ArrayList<String> bubbleSort(ArrayList<String> array) {
---
+++ coursework2/Testing.java
@@ -0,0 +1,5 @@
+public class Testing {
+    public static void main() {
+        IO.println(SortComparison.cardCompare("1S", "12H"));
+    }
+}
